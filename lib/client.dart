fd095eb43698b5a73d1c78bf8bca9c0e52e01213
Phirada Nilbai <phrada@kkumail.com>
2023-06-06 10:16:41 +0700

    Update client.dart

--- lib/client.dart
+++ lib/client.dart
@@ -1,74 +1,75 @@
 import 'dart:io';
-import 'dart:convert';
-import 'dart:typed_data';
 
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ClientApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class ClientApp extends StatelessWidget {
+  const ClientApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'TCP Test App',
-      home: MyHomePage(),
+      title: 'TCP Test App for client',
+      home: ClientPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+class ClientPage extends StatefulWidget {
+  const ClientPage({Key? key}) : super(key: key);
 
   @override
-  // ignore: library_private_types_in_public_api
-  _MyHomePageState createState() => _MyHomePageState();
+// ignore: library_private_types_in_public_api
+  _ClientPageState createState() => _ClientPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  ServerSocket? server;
+class _ClientPageState extends State<ClientPage> {
   Socket? client;
   TextEditingController messageController = TextEditingController();
-  String serverStatus = 'Server status: Not started';
   String clientStatus = 'Client status: Not connected';
   String receivedMessage = '';
-  String receivedMessageserver = '';
 
   @override
   void dispose() {
-    server?.close();
     client?.close();
     super.dispose();
   }
 
   void startClient() async {
     try {
-      client = await Socket.connect('192.168.56.1', 4100);
+      client = await Socket.connect('172.20.10.10', 4100);
       setState(() {
-        clientStatus = 'Client status: Connected Host';
+        clientStatus = 'Client status: Connected';
       });
 
       String message = messageController.text;
-      client?.write(utf8.encode(message));
+      client?.write((message));
 
-      client?.listen((Uint8List data) {
-        String response = "OK, I got it";
-        print('Received response from server: $response');
+      client?.listen((data) {
+        var response = String.fromCharCodes(data);
+// print('Received response from server: $response');
         setState(() {
-          receivedMessageserver = response;
+          receivedMessage = response;
         });
       });
     } catch (e) {
-      print('Failed to connect client: $e');
+//print('Failed to connect client: $e');
       setState(() {
         clientStatus = 'Client status: Failed';
       });
     }
+  }
+
+  void disconnectClient() {
+    client?.destroy();
+    setState(() {
+      clientStatus = 'Client status: Disconnected';
+    });
   }
 
   @override
@@ -83,22 +84,35 @@ class _MyHomePageState extends State<MyHomePage> {
             Text(clientStatus, style: GoogleFonts.prompt()),
             ElevatedButton(
               onPressed: startClient,
-              child: Text('Start Client', style: GoogleFonts.prompt()),
+              child: Text('Connect to server', style: GoogleFonts.prompt()),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: disconnectClient,
+              child: Text('Disconnect', style: GoogleFonts.prompt()),
             ),
             const SizedBox(height: 20),
             TextField(
               controller: messageController,
               decoration: InputDecoration(
-                  labelText: 'Message', hintStyle: GoogleFonts.prompt()),
+                  contentPadding: const EdgeInsets.all(20),
+                  labelText: 'Type a Message',
+                  hintStyle: GoogleFonts.prompt()),
             ),
             ElevatedButton(
-              onPressed: startClient,
-              child:
-                  Text('Send Message to Server', style: GoogleFonts.prompt()),
+              onPressed: () {
+                startClient();
+              },
+              child: Text(
+                'Send Message to Server',
+                style: GoogleFonts.prompt(),
+              ),
             ),
             const SizedBox(height: 20),
-            Text('Received Message from server: $receivedMessageserver',
-                style: GoogleFonts.prompt()),
+            Text(
+              'Received Message from server: $receivedMessage',
+              style: GoogleFonts.prompt(),
+            ),
           ],
         ),
       ),
