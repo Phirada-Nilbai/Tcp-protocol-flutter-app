ae6a0664ee8c13bee15bb3eea984e844f06a008b
Phirada Nilbai <phrada@kkumail.com>
2023-06-06 17:08:50 +0700

    Update IOS and  MacOS

--- lib/client.dart
+++ lib/client.dart
@@ -1,4 +1,8 @@
 import 'dart:io';
+<<<<<<< HEAD
+=======
+
+>>>>>>> 40dcc06aa271e3f2e1a7cc4d893bdf3d7a5a6eb0
 
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
@@ -24,7 +28,11 @@ class ClientPage extends StatefulWidget {
   const ClientPage({Key? key}) : super(key: key);
 
   @override
+<<<<<<< HEAD
 // ignore: library_private_types_in_public_api
+=======
+  // ignore: library_private_types_in_public_api
+>>>>>>> 40dcc06aa271e3f2e1a7cc4d893bdf3d7a5a6eb0
   _ClientPageState createState() => _ClientPageState();
 }
 
@@ -52,17 +60,36 @@ class _ClientPageState extends State<ClientPage> {
 
       client?.listen((data) {
         var response = String.fromCharCodes(data);
+<<<<<<< HEAD
 // print('Received response from server: $response');
+=======
+       // print('Received response from server: $response');
+>>>>>>> 40dcc06aa271e3f2e1a7cc4d893bdf3d7a5a6eb0
         setState(() {
           receivedMessage = response;
         });
       });
     } catch (e) {
+<<<<<<< HEAD
 //print('Failed to connect client: $e');
       setState(() {
         clientStatus = 'Client status: Failed';
       });
     }
+=======
+        print('Failed to connect client: $e');
+        setState(() {
+          clientStatus = 'Client status: Failed';
+        });
+    } 
+    } 
+  
+  void disconnectClient() {
+    client?.destroy();
+    setState(() {
+      clientStatus = 'Client status: Disconnected';
+    });
+>>>>>>> 40dcc06aa271e3f2e1a7cc4d893bdf3d7a5a6eb0
   }
 
   void disconnectClient() {
@@ -75,29 +102,43 @@ class _ClientPageState extends State<ClientPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Client', style: GoogleFonts.prompt())),
+      appBar: AppBar(title: Text('Client' , style: GoogleFonts.prompt())),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const SizedBox(height: 20),
-            Text(clientStatus, style: GoogleFonts.prompt()),
+            Text(clientStatus/*, style: GoogleFonts.prompt()*/),
             ElevatedButton(
               onPressed: startClient,
               child: Text('Connect to server', style: GoogleFonts.prompt()),
+<<<<<<< HEAD
             ),
             const SizedBox(height: 20),
             ElevatedButton(
               onPressed: disconnectClient,
               child: Text('Disconnect', style: GoogleFonts.prompt()),
+=======
+>>>>>>> 40dcc06aa271e3f2e1a7cc4d893bdf3d7a5a6eb0
             ),
             const SizedBox(height: 20),
+            /*ElevatedButton(
+              onPressed: disconnectClient,
+              child: Text('Disconnect', style: GoogleFonts.prompt()),
+            ), */
+            const SizedBox(height: 20),
             TextField(
+      
               controller: messageController,
               decoration: InputDecoration(
+<<<<<<< HEAD
                   contentPadding: const EdgeInsets.all(20),
                   labelText: 'Type a Message',
                   hintStyle: GoogleFonts.prompt()),
+=======
+                contentPadding: const EdgeInsets.all(20),
+                  labelText: 'Type a Message', hintStyle: GoogleFonts.prompt()),
+>>>>>>> 40dcc06aa271e3f2e1a7cc4d893bdf3d7a5a6eb0
             ),
             ElevatedButton(
               onPressed: () {
